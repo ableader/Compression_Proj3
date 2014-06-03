f48e802558be525a04c3cc039befe0325a97769b
Charles Hsueh <ckhsueh@uci.edu>
2014-06-03 10:28:04 -0700

    Another Update
    Changed how main is formatted to be more stream lined.
    Unfortunately could not fix missing byte problems

--- Huffman.h
+++ Huffman.h
@@ -1,4 +1,3 @@
-#include <string>
 #include <iostream>
 #include <fstream>
 #include <unordered_map>
@@ -195,13 +194,13 @@ public:
 	Bits encodeTree(NodePointer np, Bits BITS);
 
 	// Encode the incoming message using current HUffmantree
-	string encode(fstream & input);
+	void encode(fstream & input, fstream & output);
 
 	void buildDecodingTree(NodePointer np, Bits & BITS);
 
 	// Decode incoming message
 	// will rebuild Huffman tree and analyze incoming data
-	string decode(fstream & input);
+	void decode(fstream & input, fstream & output);
 
 private:
 	NodePointer rootNode;
